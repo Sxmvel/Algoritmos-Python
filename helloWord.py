fea1517995322ec9832a80b10d6bf77215b47498
Sxmvel <samuelres444@gmail.com>
2026-03-17 14:43:42 -0300

    Hello Word e função type()

---
+++ helloWord.py
@@ -0,0 +1,11 @@
+print("Hello, Word")
+
+idade = 23   # Python sabe que é um int
+nome = "Samuel"  # Python sabe que isso é uma String
+nota_prova = 9.5 # Python sabe que isso eh um float
+
+
+# a função type() _ retorna o tipo da variavel
+
+print(type(nome))
+
