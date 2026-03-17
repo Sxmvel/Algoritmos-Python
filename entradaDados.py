17be963e960751799d045e9e9d0af4380b43e713
Sxmvel <samuelres444@gmail.com>
2026-03-17 14:52:50 -0300

    Estrada de dados python input()

---
+++ entradaDados.py
@@ -0,0 +1,11 @@
+
+
+# Entrada de dados Função input()
+
+
+# Ler uma String normal.
+nome_time = input("Digite o nome da sua equipe: ")
+
+
+pontuacao = int(input("Digite a pontuação desejada: "))
+
